1ac349e89fa821df9b867b0b3a84f985f5cd61a5
efondo_aaronkyle <akyleefondo@gmail.com>
2025-05-31 11:53:57 +0800

    initialize variable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import random
 
+def rock_paper_scissors():
+    player_move = input("Select your move(rock, paper, or scissors): ").lower()
+    computer_move = random.choice(['rock', 'paper', 'scissors'])
+
 def main():
     pass
