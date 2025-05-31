ce1464308ff1dbca3f382bcc93a8c876e189af4e
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-05-31 11:56:34 +0800

    add def translate_tagalog function in main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from googletrans import Translator
 import asyncio
+
+def translate_tagalog(text):
+    async def do_translate():
+        translator = Translator()
+        translated = await translator.translate(text, dest='en')
+        return translated.text
+    
+    return asyncio.run(do_translate())
