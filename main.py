6aef48cae40f4c1bea0e3f0fc14094c0e86c2bad
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-05-31 11:58:18 +0800

    add several function

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ def translate_tagalog(text):
         return translated.text
     
     return asyncio.run(do_translate())
+
+tagalog_text = input("Enter text: ")
+
+english_text = translate_tagalog(tagalog_text)
+
+print(f"Translated text: {english_text}")
