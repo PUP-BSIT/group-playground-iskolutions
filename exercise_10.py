3d7e8ef32f7a65adcb99b8a52ee1aebffdee634d
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-05-31 17:55:18 +0800

    add exercise_10.py

---
+++ exercise_10.py
@@ -0,0 +1,14 @@
+fruit_count = {}
+
+while True:
+    fruit = input("Enter a fruit (type 'exit' to stop): ").lower()
+    if fruit.lower() == 'exit':
+        break
+    if fruit in fruit_count:
+        fruit_count[fruit] += 1
+    else:
+        fruit_count[fruit] = 1
+
+print("\nFinal counts:")
+for fruit, count in fruit_count.items():
+    print(f"{fruit} is {count}")
