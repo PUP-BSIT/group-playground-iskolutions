43d331522c814cc3d250a2100bb409a548728711
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-05-31 17:31:12 +0800

    remove asyncio module

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from googletrans import Translator
-import asyncio
 
 def translate_tagalog(text):
-    async def do_translate():
         translator = Translator()
-        translated = await translator.translate(text, dest='en')
+        translated = translator.translate(text, dest='en')
         return translated.text
-    
-    return asyncio.run(do_translate())
 
 tagalog_text = input("Enter text: ")
 
