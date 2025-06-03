a5ca8c194c2d8471110c0d35abfaee773c2677e7
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-06-03 20:38:15 +0800

    add display_menu function in lopez.py

--- lopez.py
+++ lopez.py
@@ -1,1 +1,10 @@
-import os
+from os import system
+
+def display_menu():
+    print("=== WELCOME TO JAKIM LOPEZ'S PROFILE ===")
+    print("1. Get to know me better")
+    print("2. Goals in life")
+    print("3. Miko's comment")
+    print("4. Kyle's comment")
+    print("5. Hanz's comment")
+    print("6. Pearl's Comment")
